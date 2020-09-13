7a2d024b217692d92ffad4f2a4dfc7314ee7c4a0
Adrian <70647811+agrichadrian@users.noreply.github.com>
2020-09-13 23:30:15 +0300

    Add files via upload

---
+++ TestAuthor.java
@@ -0,0 +1,8 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        Author author1 = new Author("Itachi Uchiha ", "itachitraitir@Akatsuki.ru", 'M');
+        System.out.println(author1);
+    }
+}
